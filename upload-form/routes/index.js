b7bfd178f9dddf664dee2dd1e8606bcd2b59280d
Noah Heath <noah.heath@evanschambers.com>
2015-05-21 22:10:59 -0400

    Upload to S3 Bucket Successful
    EAT-1

--- upload-form/routes/index.js
+++ upload-form/routes/index.js
@@ -2,13 +2,31 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var done = false;
+var AWS = require('aws-sdk');
 
-router.use(multer({ dest: './uploads/',
+var s3 = new AWS.S3();
+
+var bucketName = 'eaton-resume-bucket';
+
+router.use(multer({ 
+	dest: './uploads/',
 	rename: function (fieldname, filename) {
 		return filename + Date.now();
 	},
-	onFileUploadStart: function (file) {
+	onFileUploadStart: function (file, data, req, res) {
 		console.log(file.originalname + ' is starting ...')
+		var params = {
+			Bucket: bucketName,
+			Key: file.name,
+			Body: data
+		};
+		s3.putObject(params, function(perr, pres){
+			if (perr){
+				console.log("Error uploading data: ", perr);
+			} else {
+				console.log("Successfully uploaded data to Eaton Resume Bucket");
+			}
+		});
 	},
 	onFileUploadComplete: function(file) {
 		console.log(file.fieldname + ' uploaded to ' + file.path)
@@ -21,7 +39,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Upload Form' });
 });
 
-router.post('/file-upload', function(req, res){
+router.post('/', function(req, res){
 	if(done == true){
 		console.log(req.files);
 		res.end("File uploaded.");
