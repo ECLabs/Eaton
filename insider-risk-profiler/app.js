ba379b1da26db39afae34d90cec9ab72a99838bf
Ryan Gross <ryan.gross.01@gmail.com>
2016-04-09 14:52:43 -0400

    Just adding some raw web form elements to submit with.

--- insider-risk-profiler/app.js
+++ insider-risk-profiler/app.js
@@ -7,13 +7,15 @@ var dynamodb = new AWS.DynamoDB();
 
 app.use(express.static('app'));
 
-// respond with "Hello World!" on the homepage
+/*
+ * Add /heatbeat to base url to see if application is running
+ */
 app.get('/heartbeat', function (req, res) {
-  console.log(req.query.val);
+  //TODO add database queries and anything else to exercise all third party communications to provide a better check
   res.send('I\'m alive!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 
