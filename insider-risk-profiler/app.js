8d67b1a8f082d00aad64376f76e447df9515cc79
Ryan Gross <ryan.gross.01@gmail.com>
2016-04-09 20:30:37 -0400

    Made changes to work with AWS Elastic Beanstalk. Also created a .zip file for Elastic Beanstalk deployment.

--- insider-risk-profiler/app.js
+++ insider-risk-profiler/app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var AWS = require('aws-sdk'); 
+var http = http = require('http');
 
-var app = express();
 AWS.config.update({region:'us-east-1'});
 var dynamodb = new AWS.DynamoDB();
 
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
 app.use(express.static('app'));
 
 /*
@@ -15,9 +18,6 @@ app.get('/heartbeat', function (req, res) {
   res.send('I\'m alive!');
 });
 
-var server = app.listen(8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Server started at http://%s:%s', host, port);
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
 });
