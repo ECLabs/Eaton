f45b238eb0556db4423b7a01c631411fdb8d6f75
Ryan Gross <ryan.gross.01@gmail.com>
2016-04-14 20:55:55 -0400

    Added calendars to input form.

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	$("#submit-button").on('click', function(){
 		submit();
 	});
+	
+	setupDatePickers();
 });
 
 function submit(){
@@ -24,4 +26,29 @@ function submit(){
 	request.fail(function( jqXHR, textStatus ) {
 	  alert( "Request failed: " + textStatus );
 	});
+}
+
+function setupDatePickers(){
+	var nowTemp = new Date();
+	var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+	 
+	var checkin = $('#arrival').datepicker({
+	  // onRender: function(date) {
+	    // return date.valueOf() < now.valueOf() ? 'disabled' : '';
+	  // }
+	}).on('changeDate', function(ev) {
+	    var newDate = new Date(ev.date);
+	    newDate.setDate(newDate.getDate() + 1);
+	    checkout.setValue(newDate);
+	  checkin.hide();
+	  $('#departure')[0].focus();
+	}).data('datepicker');
+	var checkout = $('#departure').datepicker({
+	  onRender: function(date) {
+	    return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+	  }
+	}).on('changeDate', function(ev) {
+	  checkout.hide();
+	}).data('datepicker');
+
 }
